73ddb15ce84f400f0dc51793296052952856a20e
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-22 19:01:08 +0330

    Update conf.py

--- utils/conf.py
+++ utils/conf.py
@@ -32,3 +32,6 @@ def set_random_seed(seed: int) -> None:
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
